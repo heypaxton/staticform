b7848e9a2632a1276962c4bdd357ad431c3f0bf5
Eric Paxton <heypaxton@gmail.com>
2017-06-24 16:17:58 -0700

    Remove Guardian and add Coherence

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Staticformio.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:guardian, "~> 0.14"}]
+     {:coherence, "~> 0.3.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
