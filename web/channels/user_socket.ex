c6f446f03b1c5acfef213677a55e03e52dc2226e
Eric Paxton <heypaxton@gmail.com>
2017-06-24 09:46:38 -0700

    Set up production config for heroku and decrease socket timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,9 @@ defmodule Staticformio.UserSocket do
   # channel "room:*", Staticformio.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
+
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
