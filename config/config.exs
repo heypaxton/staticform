ada6489ce4aad391b039cabefcf740a3ade79e7b
Eric Paxton <heypaxton@gmail.com>
2017-06-24 14:56:41 -0700

    Add Guardian for authentication

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,16 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Configure Guardian
+config :guardian, Guardian,
+  issuer: "Statiformio",
+  ttl: { 30, :days },
+  allowed_drift: 2000,
+  verify_issuer: true,
+  secret_key: 'lBVmtXx2oZn1j+nz4tj0Ernq3OL2m8BwJ9ljhjSulewyVeLNPoWoLtqEEsYQnPpr',
+  serializer: Staticformio.GuardianSerializer
+
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
